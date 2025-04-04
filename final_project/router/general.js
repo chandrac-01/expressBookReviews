97529f7cdf2d8cb29680db78eafa03394cbd49cb
chandrac-01 <cchatterjee108@gmail.com>
2025-04-04 13:09:14 -0400

    message

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,19 +12,22 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    res.send(JSON.stringify(books,null,4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+    const isbn = req.params.isbn;
+    if(books[isbn]){
+        return res.status(200).send(JSON.stringify(books,null,4)); // Return the book details
+    }else{
+        return res.status(404).json({message: "Not found"}); // Send a 404 if book not found
+    };
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
+    const author = req.params.author;
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
